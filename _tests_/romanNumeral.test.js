d7c7bb5df0d7464cd6e321a7c8152dbf9edd865b
Paige Tiedeman <paigetiedeman@gmail.com>
2021-11-08 15:14:22 -0800

    test added for Numeral I
    Co-authored-by: Chynna Lew <chynnalew@yahoo.com>

--- _tests_/romanNumeral.test.js
+++ _tests_/romanNumeral.test.js
@@ -3,5 +3,9 @@ import convertRoman from "../src/js/scripts.js";
 describe('convertRoman', () => {
   test('should return NaN if argument is NaN', () => {
     expect(convertRoman("hello")).toEqual(NaN);
-  })
+  });
+
+  test('should return I for 1', () => {
+    expect(convertRoman(1)).toEqual("I");
+  });
 });
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,5 +1,7 @@
 
 
 export default function convertRoman(num) {
-
+  if (isNaN(num)) {
+    return NaN;
+  }
 }
